9d273ee1cb2d719c2976db32fae3e9740f7c657f
Rainy Dakus <rainy@ualberta.ca>
2026-09-03 17:58:22 -0600

    removed extraneous code

--- PetShop/app/src/main/java/com/example/petshop/MainActivity.kt
+++ PetShop/app/src/main/java/com/example/petshop/MainActivity.kt
@@ -16,17 +16,6 @@ import com.example.petshop.ui.theme.PetShopTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PetShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
         val cat = Cat("Coco", 6)
         val dog = Dog("Mochi", 6)
         val scorpion = Scorpion("Stinger", 32)
@@ -35,18 +24,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PetShopTheme {
-        Greeting("Android")
-    }
-}
